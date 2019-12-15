ce7e4e982428cdcd1e3cddc038bbe20f85bf73d4
Teri Eyenike <teyenike1@gmail.com>
2019-12-15 06:09:40 +0100

    set up for the page

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,11 +1,30 @@
-function scrollAppear(){
+function scrollAppear() {
   let introText = document.querySelector('.intro-text');
   let introPosition = introText.getBoundingClientRect().top;
   let screenPosition = window.innerHeight / 1.2;
 
-  if(introPosition < screenPosition){
-    introText.classList.add('intro-appear')
+  if (introPosition < screenPosition) {
+    introText.classList.add('intro-appear');
   }
 }
 
-window.addEventListener('scroll', scrollAppear)
+window.addEventListener('scroll', scrollAppear);
+
+const nav = document.querySelector('.navbar');
+const sectionOne = document.querySelector('.container');
+
+const sectionOneOptions = {
+  rootMargin: '-100px 0px 0px 0px'
+};
+
+const sectionOneObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      nav.classList.add('nav-scrolled');
+    } else {
+      nav.classList.remove('nav-scrolled');
+    }
+  });
+}, sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
